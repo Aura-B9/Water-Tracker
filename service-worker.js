8cb0b8c1d43fd2307abae0f0bb38ed0d0222ac6a
Aura-β9 <forfoxy027@gmail.com>
2025-11-01 22:59:41 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ self.addEventListener('install', (event) => {
         '/index.html',
         '/style.css',
         '/script.js',
-        '/icons/icon-192x192.png'
+        '/icons/icon1.png'
       ]);
     })
   );
